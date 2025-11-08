28d73fed558489b33c3a0026338a06b5c7d4dbac
youz2me <kynhun20@gachon.ac.kr>
2025-11-09 01:30:29 +0900

    [Feat] 기본 버튼 모양을 표시하는 RoundButton 클래스 구현

---
+++ src/client/view/RoundButton.java
@@ -0,0 +1,28 @@
+package client.view;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class RoundButton extends JButton {
+    public RoundButton(String text) {
+        super(text);
+        setContentAreaFilled(false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground());
+        g2.fillOval(0, 0, getWidth(), getHeight());
+
+        g2.setColor(getForeground());
+        g2.setFont(getFont());
+        FontMetrics fm = g2.getFontMetrics();
+        int x = (getWidth() - fm.stringWidth(getText())) / 2;
+        int y = ((getHeight() - fm.getHeight()) / 2) + fm.getAscent();
+        g2.drawString(getText(), x, y);
+
+        g2.dispose();
+    }
+}
